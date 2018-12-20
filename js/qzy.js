6775cdce2e9d17cb4d86e7381d36a3b439ea3365
屈儿 <834276365@qq.com>
2018-12-20 23:32:07 +0800

    xianlutu

--- js/qzy.js
+++ js/qzy.js
@@ -19,6 +19,7 @@ new BMap.Point(118.820486, 32.117184)
 { strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
 );
 map.addOverlay(polyline);
+//1101路线
 var polyline = new BMap.Polyline([
 new BMap.Point(118.895116, 32.050862),
 new BMap.Point(118.901810, 32.056877)
@@ -40,3 +41,247 @@ new BMap.Point(118.901004, 32.056328)
 { strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
 );
 map.addOverlay(polyline);
+//1201路线
+var polyline = new BMap.Polyline([
+new BMap.Point(118.986526, 32.126561),
+new BMap.Point(118.994913, 32.126274)
+],
+{ strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+map.addOverlay(polyline);
+var polyline = new BMap.Polyline([
+new BMap.Point(118.994913, 32.126274),
+new BMap.Point(118.992538, 32.127719)
+],
+    { strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+map.addOverlay(polyline);
+var polyline = new BMap.Polyline([
+new BMap.Point(118.992538, 32.127719),
+new BMap.Point(118.993535, 32.126262)
+],
+    { strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+map.addOverlay(polyline);
+var polyline = new BMap.Polyline([
+new BMap.Point(118.993535, 32.126262),
+new BMap.Point(118.992498, 32.127648)
+],
+    { strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+map.addOverlay(polyline);
+//1301路线
+var polyline = new BMap.Polyline([
+new BMap.Point(118.955669, 32.151090),
+new BMap.Point(118.923225, 32.100099)
+],
+    { strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+map.addOverlay(polyline);
+var polyline = new BMap.Polyline([
+new BMap.Point(118.923225, 32.100099),
+new BMap.Point(118.929337, 32.106705)
+],
+    { strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+map.addOverlay(polyline);
+var polyline = new BMap.Polyline([
+new BMap.Point(118.929337, 32.106705),
+new BMap.Point(118.943848, 32.103931)
+],
+    { strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+map.addOverlay(polyline);
+//2101路线
+var polyline = new BMap.Polyline([
+new BMap.Point(118.943353, 32.112223),
+new BMap.Point(118.908978, 32.097554)
+],
+    { strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+map.addOverlay(polyline);
+var polyline = new BMap.Polyline([
+new BMap.Point(118.908978, 32.097554),
+new BMap.Point(118.915785, 32.103545)
+],
+    { strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+map.addOverlay(polyline);
+var polyline = new BMap.Polyline([
+new BMap.Point(118.915785, 32.103345),
+new BMap.Point(118.915590, 32.103414)
+],
+    { strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+map.addOverlay(polyline);
+//2201路线
+var polyline = new BMap.Polyline([
+new BMap.Point(118.985903, 32.122109),
+new BMap.Point(118.992498, 32.127648)
+],
+    { strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+map.addOverlay(polyline);
+var polyline = new BMap.Polyline([
+new BMap.Point(118.992498, 32.127648),
+new BMap.Point(118.992538, 32.127719)
+],
+    { strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+map.addOverlay(polyline);
+var polyline = new BMap.Polyline([
+new BMap.Point(118.992538, 32.127719),
+new BMap.Point(118.928525, 32.107726)
+],
+    { strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+map.addOverlay(polyline);
+var polyline = new BMap.Polyline([
+new BMap.Point(118.928525, 32.107726),
+new BMap.Point(118.929631, 32.105761)
+],
+    { strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+map.addOverlay(polyline);
+//2301路线
+var polyline = new BMap.Polyline([
+new BMap.Point(118.955669, 32.151090),
+new BMap.Point(118.965238, 32.158332)
+],
+    { strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+map.addOverlay(polyline);
+var polyline = new BMap.Polyline([
+new BMap.Point(118.965238, 32.158332),
+new BMap.Point(118.965275, 32.157820)
+],
+    { strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+map.addOverlay(polyline);
+var polyline = new BMap.Polyline([
+new BMap.Point(118.965275, 32.157820),
+new BMap.Point(118.992538, 32.127719)
+],
+    { strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+map.addOverlay(polyline);
+var polyline = new BMap.Polyline([
+new BMap.Point(118.992538, 32.127719),
+new BMap.Point(118.985903, 32.122109)
+],
+    { strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+map.addOverlay(polyline);
+//3101路线
+var polyline = new BMap.Polyline([
+new BMap.Point(118.803277, 32.154106),
+new BMap.Point(118.815034, 32.109938)
+],
+    { strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+map.addOverlay(polyline);
+var polyline = new BMap.Polyline([
+new BMap.Point(118.815034, 32.109938),
+new BMap.Point(118.818982, 32.112931)
+],
+    { strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+map.addOverlay(polyline);
+var polyline = new BMap.Polyline([
+new BMap.Point(118.818982, 32.112931),
+new BMap.Point(118.826786, 32.120075)
+],
+    { strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+map.addOverlay(polyline);
+//3201路线
+var polyline = new BMap.Polyline([
+new BMap.Point(118.929631, 32.105761),
+new BMap.Point(118.923225, 32.100099)
+],
+    { strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+map.addOverlay(polyline);
+var polyline = new BMap.Polyline([
+new BMap.Point(118.923225, 32.100099),
+new BMap.Point(118.933079, 32.107585)
+],
+    { strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+map.addOverlay(polyline);
+var polyline = new BMap.Polyline([
+new BMap.Point(118.933079, 32.107585),
+new BMap.Point(118.923547, 32.098900)
+],
+    { strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+map.addOverlay(polyline);
+var polyline = new BMap.Polyline([
+new BMap.Point(118.923547, 32.098900),
+new BMap.Point(118.915590, 32.103414)
+],
+    { strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+map.addOverlay(polyline);
+//3301路线
+var polyline = new BMap.Polyline([
+new BMap.Point(118.986526,32.126561),
+new BMap.Point(118.929631,32.105761)
+],
+{ strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+map.addOverlay(polyline);
+
+var polyline = new BMap.Polyline([
+new BMap.Point(118.929631,32.105761),
+new BMap.Point(118.929546,32.106421)
+],
+{ strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+map.addOverlay(polyline);
+var polyline = new BMap.Polyline([
+new BMap.Point(118.929546,32.106421),
+new BMap.Point(118.923225,32.100099)
+],
+{ strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+map.addOverlay(polyline);
+//4101路线
+var polyline = new BMap.Polyline([
+new BMap.Point(118.803277,32.154106),
+new BMap.Point(118.822117,32.207718)
+],
+{ strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+map.addOverlay(polyline);
+var polyline = new BMap.Polyline([
+new BMap.Point(118.822117,32.207718),
+new BMap.Point(118.818864,32.181397)
+],
+{ strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+map.addOverlay(polyline);
+//4201路线
+var polyline = new BMap.Polyline([
+new BMap.Point(118.960047,32.151708),
+new BMap.Point(118.964317,32.155155)
+],
+{ strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+map.addOverlay(polyline);
+var polyline = new BMap.Polyline([
+new BMap.Point(118.964317,32.155155),
+new BMap.Point(118.955669,32.15109)
+],
+{ strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+map.addOverlay(polyline);
+var polyline = new BMap.Polyline([
+new BMap.Point(118.955669,32.15109),
+new BMap.Point(118.965238,32.158332)
+],
+{ strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+map.addOverlay(polyline);
+//4301路线
+
