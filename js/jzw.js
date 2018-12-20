e3397ec5404b498e3c57da8a3bfe5cde8c994b63
Nakido <yuyukosaigyouji@foxmail.com>
2018-12-20 22:42:08 +0800

    Separated JSs

---
+++ js/jzw.js
@@ -0,0 +1,42 @@
+﻿var polyline = new BMap.Polyline([
+new BMap.Point(118.817139, 32.146839),
+new BMap.Point(118.795552, 32.127325)
+],
+{ strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+);
+map.addOverlay(polyline);
+var polyline = new BMap.Polyline([
+new BMap.Point(118.795552, 32.127325),
+new BMap.Point(118.815034, 32.109938)
+],
+{ strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+);
+map.addOverlay(polyline);
+var polyline = new BMap.Polyline([
+new BMap.Point(118.815034, 32.109938),
+new BMap.Point(118.820486, 32.117184)
+],
+{ strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+);
+map.addOverlay(polyline);
+var polyline = new BMap.Polyline([
+new BMap.Point(118.895116, 32.050862),
+new BMap.Point(118.901810, 32.056877)
+],
+{ strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+);
+map.addOverlay(polyline);
+var polyline = new BMap.Polyline([
+new BMap.Point(118.901810, 32.056877),
+new BMap.Point(118.902530, 32.058679)
+],
+{ strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+);
+map.addOverlay(polyline);
+var polyline = new BMap.Polyline([
+new BMap.Point(118.902530, 32.058679),
+new BMap.Point(118.901004, 32.056328)
+],
+{ strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+);
+map.addOverlay(polyline);
