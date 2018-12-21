0c237c2293096751166016c6d5b9834d3fbf8566
Nakido <yuyukosaigyouji@foxmail.com>
2018-12-21 13:55:35 +0800

    1

--- js/jzw.js
+++ js/jzw.js
@@ -1,12 +1,1 @@
-﻿// 百度地图API功能
-var map = new BMap.Map("allmap");    // 创建Map实例
-map.centerAndZoom(new BMap.Point(118.779, 32.044), 11);  // 初始化地图,设置中心点坐标和地图级别
-//添加地图类型控件
-map.addControl(new BMap.MapTypeControl({
-    mapTypes: [
-        BMAP_NORMAL_MAP,
-        BMAP_HYBRID_MAP
-    ]
-}));
-map.setCurrentCity("南京");          // 设置地图显示的城市 此项是必须设置的
-map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+﻿
