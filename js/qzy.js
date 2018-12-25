08ddaa6445b64657c627c9201552e6aafbd545e9
屈儿 <834276365@qq.com>
2018-12-25 19:45:25 +0800

    1234

--- js/qzy.js
+++ js/qzy.js
@@ -1,10 +1,4 @@
-﻿//创建点对象
-var myP1 = new BMap.Point(118.817139,32.146839);    //1101
-var myP2 = new BMap.Point(118.795552,32.127325);    //1102
-var myP3 = new BMap.Point(118.815034,32.109938);    //1103
-var myP4 = new BMap.Point(118.820486,32.117184);    //1104
-
-//创建驾车实例
+﻿//创建驾车实例
 var driving = new BMap.DrivingRoute(map, {
     renderOptions: {
         map: map,
@@ -12,8 +6,15 @@ var driving = new BMap.DrivingRoute(map, {
     }
 });
 
+//创建点对象
+var myP1 = new BMap.Point(118.817139, 32.146839);    //1101
+var myP2 = new BMap.Point(118.795552, 32.127325);    //1102
+var myP3 = new BMap.Point(118.815034, 32.109938);    //1103
+var myP4 = new BMap.Point(118.820486, 32.117184);    //1104
+
+
 //创建驾车搜索
-driving.search(p1, p4);//waypoints表示途经点        
+driving.search(myp1, myp4, { waypoints: [myp2, myp3] });//waypoints表示途经点
  
 
 //绘制折线
@@ -21,7 +22,7 @@ driving.setSearchCompleteCallback(function(){
     var pts = driving.getResults().getPlan(0).getRoute(0).getPath();    //通过驾车实例，获得一系列点的数组
 
     var polyline = new BMap.Polyline([pts],
-{ strokeColor: "red", strokeWeight: 6, strokeOpacity: 1 }
+{ strokeColor: "red", strokeWeight: 6, strokeOpacity: 0.5 }
    );
     map.addOverlay(polyline);
 })
