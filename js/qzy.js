bf995e10af78273303429056321e1992cb474773
屈儿 <834276365@qq.com>
2018-12-28 22:42:04 +0800

    123

--- js/qzy.js
+++ js/qzy.js
@@ -1,5 +1,5 @@
 ﻿//创建驾车实例
-var driving1 = new BMap.DrivingRoute(map, {
+var driving = new BMap.DrivingRoute(map, {
     renderOptions: {
         map: map,
         autoViewport: true
@@ -14,12 +14,12 @@ var myp4 = new BMap.Point(118.820486, 32.117184);    //1104
 
 
 //创建驾车搜索
-driving1.search(myp1, myp4, { waypoints: [myp2, myp3] });//waypoints表示途经点
+driving.search(myp1, myp4, { waypoints: [myp2, myp3] });//waypoints表示途经点
 
 
 //绘制折线
-driving1.setSearchCompleteCallback(function () {
-    var pts = driving1.getResults().getPlan(0).getRoute(0).getPath();    //通过驾车实例，获得一系列点的数组
+driving.setSearchCompleteCallback(function () {
+    var pts = driving.getResults().getPlan(0).getRoute(0).getPath();    //通过驾车实例，获得一系列点的数组
 
     var polyline = new BMap.Polyline([pts],
 { strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
@@ -52,7 +52,7 @@ var route_y = new Array("32.146839");
 var route_title = new Array("江畔一日游");
 var route_discribe1 = new Array("紧邻长江，将长江边上的燕子矶风光和长江观音风光一览无余之后，/n在城北的迈皋桥吃上一顿热火朝天的火锅，再到不远处的彩虹广场逛逛，美好的一天就这样结束了！");
 
-function addroute(point, i) {
+function addroute(point) {
     
     //添加图标点击事件
     lab1.addEventListener("click", function () {
@@ -61,8 +61,8 @@ function addroute(point, i) {
             height: 500,     // 信息窗口高度    
         }
         //添加信息窗口内容
-        var content1 = '<div class="point_content_title">' + route_title[i] + '</div>' + '<br/>' +
-                '<div class="point_content_discribe">' + '路线简介: ' + route_discribe1[i] ;
+        var content1 = '<div class="point_content_title">' + route_title + '</div>' + '<br/>' +
+                '<div class="point_content_discribe">' + '路线简介: ' + route_discribe1 ;
         // 创建信息窗口对象 		
         var infoWindow1 = new BMap.InfoWindow(content1, opts1);
         // 打开信息窗口   
