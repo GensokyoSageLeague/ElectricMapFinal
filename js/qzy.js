1b55f5ee7c26114dcdb6542bc2316c2dd1cdad65
屈儿 <834276365@qq.com>
2018-12-29 21:13:59 +0800

    1

--- js/qzy.js
+++ js/qzy.js
@@ -38,31 +38,19 @@
             })
             map.addOverlay(myline);
         }
-        var myIcon = new BMap.Icon("一站式路线图标/110/1101.png", new BMap.Size(48, 48), {
-            anchor: new BMap.Size(10, 25),
-            imageOffset: new BMap.Size(0, 0)
-        });
+        var myIcon = new BMap.Icon("一站式路线图标/110/1101.png", new BMap.Size(100, 100)) ;
         var marker = new BMap.Marker(myp1, { icon: myIcon });
         map.addOverlay(marker);
 
-        var myIcon2 = new BMap.Icon("一站式路线图标/110/1102.png", new BMap.Size(48, 48), {
-            anchor: new BMap.Size(10, 25),
-            imageOffset: new BMap.Size(0, 0)
-        });
+        var myIcon2 = new BMap.Icon("一站式路线图标/110/1102.png", new BMap.Size(100, 100));
         var marker2 = new BMap.Marker(myp2, { icon: myIcon });
         map.addOverlay(marker2);
 
-        var myIcon3 = new BMap.Icon("一站式路线图标/110/1103.png", new BMap.Size(48, 48), {
-            anchor: new BMap.Size(10, 25),
-            imageOffset: new BMap.Size(0, 0)
-        });
+        var myIcon3 = new BMap.Icon("一站式路线图标/110/1103.png", new BMap.Size(100, 100));
         var marker3 = new BMap.Marker(myp3, { icon: myIcon });
         map.addOverlay(marker3);
 
-        var myIcon4 = new BMap.Icon("一站式路线图标/110/1104.png", new BMap.Size(48, 48), {
-            anchor: new BMap.Size(10, 25),
-            imageOffset: new BMap.Size(0, 0)
-        });
+        var myIcon4 = new BMap.Icon("一站式路线图标/110/1104.png", new BMap.Size(100, 100));
         var marker4 = new BMap.Marker(myp4, { icon: myIcon });
         map.addOverlay(marker4);
         
