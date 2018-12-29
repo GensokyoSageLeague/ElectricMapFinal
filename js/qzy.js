4eb61ac532b0d0c54e5475a6e86e12355f4bcf05
屈儿 <834276365@qq.com>
2018-12-29 19:12:00 +0800

    2

--- js/qzy.js
+++ js/qzy.js
@@ -30,7 +30,7 @@
                 }
                 //添加信息窗口内容
                 var content = '<div class="point_content_title">' + '江畔一日游\t评分：AAAA' + '</div>' + '<br/>' + '<div class="point_content_discribe">' + '攻略简介：紧邻长江，将长江边上的燕子矶风光和长江观音风光一览无余之后，在城北的迈皋桥吃上一顿热火朝天的火锅，\n再到不远处的彩虹广场逛逛，美好的一天就这样结束了!' + '</div>' + '<br/>' +
-                   '<div class="point_content_discribe">' + '路线：燕子矶公园-长江观音景区-重庆捞翻天火锅-晓庄国际彩虹广场' + '</div>' + '<br/>' + '<div class="point_content_discribe">' + '  祝您本次出行愉快，欢迎您在行程结束时返回页面为本次路线打分！'+ '</div>' + '<br/'+'<div class="point_content_picture"><img src = '+Users/dell/Desktop/zhufu.gif+' class="1picture" width="150px" height="300"><div>';
+                   '<div class="point_content_discribe">' + '路线：燕子矶公园-长江观音景区-重庆捞翻天火锅-晓庄国际彩虹广场' + '</div>' + '<br/>' ;
                 // 创建信息窗口对象 		
                 var infoWindow = new BMap.InfoWindow(content, opts);
                 // 打开信息窗口   
@@ -51,3 +51,59 @@
 }
 
 addroot1();
+
+function addroot2() {
+    //创建驾车实例
+    var driving1 = new BMap.DrivingRoute(map, {
+        renderOptions: {
+            map: map,
+            autoViewport: true
+        }
+    });
+
+    //创建点对象
+    var myp5 = new BMap.Point(118.895116, 32.050862);
+    var myp6 = new BMap.Point(118.901810, 32.056827);
+    var myp7 = new BMap.Point(118.902530, 32.058679);
+    var myp8 = new BMap.Point(118.901004, 32.056328);
+    var pts1;
+
+    //创建驾车搜索
+    driving1.search(myp5, myp8, { waypoints: [myp6, myp7] });//waypoints表示途经点
+    //回调搜索的路线
+    driving.setPolylinesSetCallback(function (lines) {
+        addinformation(lines);
+    })
+    //处理回调的路线
+    function addinformation(lines) {
+        for (var line in lines) {
+            myline = lines[line].getPolyline();
+            myline.addEventListener("click", function () {
+                var opts = {
+                    width: 200,     // 信息窗口宽度    
+                    height: 550,     // 信息窗口高度    
+                }
+                //添加信息窗口内容
+                var content = '<div class="point_content_title">' + '马群周边游\t评分：AAAA' + '</div>' + '<br/>' + '<div class="point_content_discribe">' + '攻略简介：这条线路适合生活在马群附近的朋友们，在钟山风景名胜区走走，\n晌午，到花园城吃一餐晋家门。走了一天肯定累了，再去按按脚吧。休息片刻，还有一场精彩绝伦的电影等着你和你的爱人！' + '</div>' + '<br/>' +
+                   '<div class="point_content_discribe">' + '路线：花园城-幸福蓝海国际影城-水沐足韵-晋家门' + '</div>' + '<br/>';
+                // 创建信息窗口对象 		
+                var infoWindow = new BMap.InfoWindow(content, opts);
+                // 打开信息窗口   
+                map.openInfoWindow(infoWindow, map.getCenter());
+            })
+            map.addOverlay(myline);
+        }
+    }
+
+    var lab5 = new BMap.Label("花园城", { position: myp5 });        //创建4个label
+    var lab6 = new BMap.Label("幸福蓝海国际影城", { position: myp6 });
+    var lab7 = new BMap.Label("水沐足韵", { position: myp7 });
+    var lab8 = new BMap.Label("晋家门", { position: myp8 });
+    map.addOverlay(lab5);
+    map.addOverlay(lab6);
+    map.addOverlay(lab7);
+    map.addOverlay(lab8);
+
+}
+
+addroot2();
