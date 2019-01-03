ecc6258d9faf1688fc4aa22e685d4af5eb5f9523
璇儿 <shaonvxinxuan@outlook.com>
2019-01-04 00:10:40 +0800

    zzxx

--- works/js/others.extended.js
+++ works/js/others.extended.js
@@ -45,19 +45,19 @@ var menus = [{
 	id: 2,
 	icon: "img/2.png",
 	title: "玩乐",
-	cb: callback
+	cb: callback2
 },
 {
 	id: 3,
 	icon: "img/3.png",
 	title: "景点",
-	cb: callback
+	cb: callback3
 },
 {
 	id: 4,
 	icon: "img/4.png",
 	title: "购物",
-	cb: callback
+	cb: callback4
 },
 {
 	id: 5,
@@ -74,6 +74,15 @@ var menus = [{
 // 环形菜单回调函数
 function callback1(ele, data) {
     panduanfood();
+}
+function callback2(ele, data) {
+    panduanplay();
+}
+function callback3(ele, data) {
+    panduanscene();
+}
+function callback4(ele, data) {
+    panduanshop();
 }
 var content_nakido_def;
 function callback5(ele, data) {
@@ -130,18 +139,240 @@ function searchevent() {
 	});
 	local.search(myValue);
 }
+//判断在圆内的吃喝
 function panduanfood() {
     //添加我们的point
     var foodpoint = [
         new BMap.Point(118.92319, 32.098621),
         new BMap.Point(118.897053, 32.138898),
         new BMap.Point(118.922949, 32.099666),
+        new BMap.Point(118.933211, 32.106362),
+        new BMap.Point(118.922561, 32.099716),
+        new BMap.Point(118.92203, 32.100406),
+        new BMap.Point(118.923793, 32.098557),
+        new BMap.Point(118.929337, 32.106705),
+        new BMap.Point(118.902542, 32.059818),
+        new BMap.Point(118.81985, 32.109476),
+        new BMap.Point(118.832943, 32.108481),
+        new BMap.Point(118.992538, 32.127719),
+        new BMap.Point(118.921267, 32.100146),
+        new BMap.Point(118.902032, 32.056978),
+        new BMap.Point(118.932428, 32.109136),
+        new BMap.Point(118.894357, 32.133938),
+        new BMap.Point(118.915556, 32.103929),
+        new BMap.Point(118.900818, 32.057046),
+        new BMap.Point(118.914316, 32.103286),
+        new BMap.Point(118.922704, 32.100009),
+        new BMap.Point(118.929546, 32.106421),
+        new BMap.Point(118.991886, 32.126778),
+        new BMap.Point(118.915536, 32.086418),
+        new BMap.Point(118.929389, 32.106733),
+        new BMap.Point(118.901004, 32.056328),
+        new BMap.Point(118.922519, 32.100214),
+        new BMap.Point(118.927994, 32.107687),
+        new BMap.Point(118.902515, 32.059015),
+        new BMap.Point(118.933079, 32.107585),
+        new BMap.Point(118.815034, 32.109938),
+        new BMap.Point(118.997847, 32.126983),
+        new BMap.Point(118.992299, 32.127598),
+        new BMap.Point(118.932714, 32.108706),
+        new BMap.Point(118.931122, 32.126573),
+        new BMap.Point(118.921038, 32.100589),
+        new BMap.Point(118.994913, 32.126274),
+        new BMap.Point(118.929514, 32.103246),
+        new BMap.Point(118.915178, 32.098359),
+        new BMap.Point(118.929225, 32.106543),
+        new BMap.Point(118.992499, 32.127617),
+        new BMap.Point(118.992862, 32.127733),
+        new BMap.Point(118.86375, 32.116754),
+        new BMap.Point(118.880228, 32.129286),
+        new BMap.Point(118.932676, 32.108721),
+        new BMap.Point(118.915242, 32.104205),
+        new BMap.Point(118.993535, 32.126262),
+        new BMap.Point(118.965238, 32.158332),
+        new BMap.Point(118.818864, 32.181397),
+
     ];
     //判断点是否在圆内
     for (var i = 0, pointsLen = foodpoint.length; i < pointsLen; i++) {
         if (BMapLib.GeoUtils.isPointInCircle(foodpoint[i], mycircle)) {
-            var marker = new BMap.Marker(foodpoint[i]); //将点转化成标注点
-            map.addOverlay(marker);  //将标注点添加到地图上
+          //替换点的图标
+                if ((i >= 0 && i < 5) || i == 26 || i == 27 || i == 46) {
+                    myIcon = new BMap.Icon("img/icon/面包甜点.png", new BMap.Size(48, 48), {
+                        anchor: new BMap.Size(10, 25),
+                        imageOffset: new BMap.Size(0, 0)
+                    });
+                    var point = new BMap.Point(foodpoint_x[i], foodpoint_y[i]);
+                    addfood(point, i);
+                }
+                else if ((i >= 5 && i < 10) || i == 28 || i == 29 || i == 30 || i == 48) {
+                    myIcon = new BMap.Icon("img/icon/火锅.png", new BMap.Size(48, 48), {
+                        anchor: new BMap.Size(10, 25),
+                        imageOffset: new BMap.Size(0, 0)
+                    });
+                    var point = new BMap.Point(foodpoint_x[i], foodpoint_y[i]);
+                    addfood(point, i);
+                }
+                else if ((i >= 10 && i < 15) || i == 35 || i == 42 || i == 43) {
+                    myIcon = new BMap.Icon("img/icon/烧烤.png", new BMap.Size(48, 48), {
+                        anchor: new BMap.Size(10, 25),
+                        imageOffset: new BMap.Size(0, 0)
+                    });
+                    var point = new BMap.Point(foodpoint_x[i], foodpoint_y[i]);
+                    addfood(point, i);
+                }
+                else if (i >= 15 && i < 20) {
+                    myIcon = new BMap.Icon("img/icon/快餐.png", new BMap.Size(48, 48), {
+                        anchor: new BMap.Size(10, 25),
+                        imageOffset: new BMap.Size(0, 0)
+                    });
+                    var point = new BMap.Point(foodpoint_x[i], foodpoint_y[i]);
+                    addfood(point, i);
+                }
+                else if ((i >= 20 && i < 25) || i == 39 || i == 40 || i == 41 || i == 44 || i == 45) {
+                    myIcon = new BMap.Icon("img/icon/地方菜.png", new BMap.Size(48, 48), {
+                        anchor: new BMap.Size(10, 25),
+                        imageOffset: new BMap.Size(0, 0)
+                    });
+                    var point = new BMap.Point(foodpoint_x[i], foodpoint_y[i]);
+                    addfood(point, i);
+                }
+                else if (i == 31 || i == 25) {
+                    myIcon = new BMap.Icon("img/icon/地方菜.png", new BMap.Size(48, 48), {
+                        anchor: new BMap.Size(10, 25),
+                        imageOffset: new BMap.Size(0, 0)
+                    });
+                    var point = new BMap.Point(foodpoint_x[i], foodpoint_y[i]);
+                    addfood(point, i);
+                }
+
+                else if (i == 47) {
+                    myIcon = new BMap.Icon("img/icon/素食.png", new BMap.Size(48, 48), {
+                        anchor: new BMap.Size(10, 25),
+                        imageOffset: new BMap.Size(0, 0)
+                    });
+                    var point = new BMap.Point(foodpoint_x[i], foodpoint_y[i]);
+                    addfood(point, i);
+                }
+
+                else {
+                    myIcon = new BMap.Icon("img/icon/外国菜.png", new BMap.Size(48, 48), {
+                        anchor: new BMap.Size(10, 25),
+                        imageOffset: new BMap.Size(0, 0)
+                    });
+                    var point = new BMap.Point(foodpoint_x[i], foodpoint_y[i]);
+                    addfood(point, i);
+                }
+            }
+        }
+}
+//判断在圆内的玩乐
+function panduanplay() {
+    var playpoint = [
+        //数据未添加
+    ];
+    for (var i = 0, pointsLen = playpoint.length; i < pointsLen; i++) {
+        if (BMapLib.GeoUtils.isPointInCircle(playpoint[i], mycircle)) {
+          if (i >= 0 && i < 5) {
+                    myIcon = new BMap.Icon("img/icon/电影院.png", new BMap.Size(48, 48), {
+                        anchor: new BMap.Size(10, 25),
+                        imageOffset: new BMap.Size(0, 0)
+                    });
+                    var point = new BMap.Point(playpoint_x[i], playpoint_y[i]);
+                    addplay(point, i);
+                }
+                else if (i >= 5 && i < 10) {
+                    myIcon = new BMap.Icon("img/icon/KTV.png", new BMap.Size(48, 48), {
+                        anchor: new BMap.Size(10, 25),
+                        imageOffset: new BMap.Size(0, 0)
+                    });
+                    var point = new BMap.Point(playpoint_x[i], playpoint_y[i]);
+                    addplay(point, i);
+                }
+                else if (i == 10) {
+                    myIcon = new BMap.Icon("img/icon/真人CS.png", new BMap.Size(48, 48), {
+                        anchor: new BMap.Size(10, 25),
+                        imageOffset: new BMap.Size(0, 0)
+                    });
+                    var point = new BMap.Point(playpoint_x[i], playpoint_y[i]);
+                    addplay(point, i);
+                }
+                else if (i == 11 || i == 12) {
+                    myIcon = new BMap.Icon("img/icon/推理类.png", new BMap.Size(48, 48), {
+                        anchor: new BMap.Size(10, 25),
+                        imageOffset: new BMap.Size(0, 0)
+                    });
+                    var point = new BMap.Point(playpoint_x[i], playpoint_y[i]);
+                    addplay(point, i);
+                }
+                else if (i >= 13 && i < 18) {
+                    myIcon = new BMap.Icon("img/icon/养生类.png", new BMap.Size(48, 48), {
+                        anchor: new BMap.Size(10, 25),
+                        imageOffset: new BMap.Size(0, 0)
+                    });
+                    var point = new BMap.Point(playpoint_x[i], playpoint_y[i]);
+                    addplay(point, i);
+                }
+                else if (i >= 18 && i < 20) {
+                    myIcon = new BMap.Icon("img/icon/酒吧.png", new BMap.Size(48, 48), {
+                        anchor: new BMap.Size(10, 25),
+                        imageOffset: new BMap.Size(0, 0)
+                    });
+                    var point = new BMap.Point(playpoint_x[i], playpoint_y[i]);
+                    addplay(point, i);
+                }
+                else if (i >= 20 && i < 23) {
+                    myIcon = new BMap.Icon("img/icon/网吧.png", new BMap.Size(48, 48), {
+                        anchor: new BMap.Size(10, 25),
+                        imageOffset: new BMap.Size(0, 0)
+                    });
+                    var point = new BMap.Point(playpoint_x[i], playpoint_y[i]);
+                    addplay(point, i);
+                }
+                else if (i == 24) {
+                    myIcon = new BMap.Icon("img/icon/茶吧.png", new BMap.Size(48, 48), {
+                        anchor: new BMap.Size(10, 25),
+                        imageOffset: new BMap.Size(0, 0)
+                    });
+                    var point = new BMap.Point(playpoint_x[i], playpoint_y[i]);
+                    addplay(point, i);
+                }
+                else {
+                    myIcon = new BMap.Icon("img/icon/游戏城.png", new BMap.Size(48, 48), {
+                        anchor: new BMap.Size(10, 25),
+                        imageOffset: new BMap.Size(0, 0)
+                    });
+                    var point = new BMap.Point(playpoint_x[i], playpoint_y[i]);
+                    addplay(point, i);
+                }
+        }
+    }
+}
+//判断在圆内的景点
+function panduanscene() {
+    var scenepoint = [
+        //数据未添加
+    ];
+    for (var i = 0, pointsLen = scenepoint.length; i < pointsLen; i++) {
+        if (BMapLib.GeoUtils.isPointInCircle(scenepoint[i], mycircle)) { 
+            myIcon = new BMap.Icon("img/icon/景区.png", new BMap.Size(48, 48), {
+                anchor: new BMap.Size(10, 25),
+                imageOffset: new BMap.Size(0, 0)
+            });
+        }
+    }
+}
+//判断在圆内的购物类
+function panduanshop() {
+    var shoppoint = [
+        //数据未添加
+    ];
+    for (var i = 0, pointsLen = shoppoint.length; i < pointsLen; i++) {
+        if (BMapLib.GeoUtils.isPointInCircle(shoppoint[i], mycircle)) {
+                myIcon = new BMap.Icon("img/icon/商场.png", new BMap.Size(48, 48), {
+                    anchor: new BMap.Size(10, 25),
+                    imageOffset: new BMap.Size(0, 0)
+                });
         }
     }
 }
